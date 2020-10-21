db1b271288bae9b5ecd835502cdbb9da3b71a98b
CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>
2020-10-21 14:26:07 -0700

    Fix server side crash

--- src/main/java/net/oriondevcorgitaco/unearthed/util/RegistrationHelper.java
+++ src/main/java/net/oriondevcorgitaco/unearthed/util/RegistrationHelper.java
@@ -6,19 +6,18 @@ import net.minecraft.util.registry.WorldGenRegistries;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.IFeatureConfig;
-import net.minecraftforge.registries.ForgeRegistries;
 import net.oriondevcorgitaco.unearthed.Unearthed;
 
 public class RegistrationHelper {
 
     public static ConfiguredFeature<?, ?> newConfiguredFeature(String registryName, ConfiguredFeature<?, ?> configuredFeature) {
-        if (!WorldGenRegistries.field_243653_e.containsKey(new ResourceLocation(Unearthed.MOD_ID, registryName)))
+        if (!WorldGenRegistries.field_243653_e.keySet().contains(new ResourceLocation(Unearthed.MOD_ID, registryName)))
             Registry.register(WorldGenRegistries.field_243653_e, new ResourceLocation(Unearthed.MOD_ID, registryName), configuredFeature);
         return configuredFeature;
     }
 
     public static <T extends IFeatureConfig, G extends Feature<T>> G registerFeature(String registryName, G feature) {
-        if (!ForgeRegistries.FEATURES.containsKey(new ResourceLocation(Unearthed.MOD_ID, registryName)))
+        if (!Registry.FEATURE.keySet().contains(new ResourceLocation(Unearthed.MOD_ID, registryName)))
             feature.setRegistryName(new ResourceLocation(Unearthed.MOD_ID, registryName));
         return feature;
     }
