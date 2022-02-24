10b6b026a0f0dd6fe170bc37b758ea394403985d
lilypuree <000aczyx@naver.com>
2022-02-24 12:18:46 +0900

    quick fix for ores not dropping xp on forge?

--- Common/src/main/java/lilypuree/unearthed/block/UEOreBlock.java
+++ Common/src/main/java/lilypuree/unearthed/block/UEOreBlock.java
@@ -5,11 +5,14 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.Mth;
+import net.minecraft.world.entity.ExperienceOrb;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.block.OreBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 
 import java.util.Random;
 
@@ -39,5 +42,12 @@ public class UEOreBlock extends OreBlock {
             return Mth.nextInt(random, 2, 5);
         } else
             return 0;
+    }
+
+    @Override
+    protected void popExperience(ServerLevel serverLevel, BlockPos pos, int amount) {
+        if (serverLevel.getGameRules().getBoolean(GameRules.RULE_DOBLOCKDROPS)) {
+            ExperienceOrb.award(serverLevel, Vec3.atCenterOf(pos), amount);
+        }
     }
 }
