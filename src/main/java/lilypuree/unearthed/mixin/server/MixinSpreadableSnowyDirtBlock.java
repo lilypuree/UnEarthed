31330922d80de8cfdafe2973bbef5c9dfc22a996
Mysticpasta1 <ethana5tc@outlook.com>
2023-07-27 06:12:03 -0500

    fix the jar

--- src/main/java/lilypuree/unearthed/mixin/server/MixinSpreadableSnowyDirtBlock.java
+++ src/main/java/lilypuree/unearthed/mixin/server/MixinSpreadableSnowyDirtBlock.java
@@ -15,13 +15,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import java.util.Random;
-
 @Mixin(SpreadingSnowyDirtBlock.class)
 public class MixinSpreadableSnowyDirtBlock {
 
     @Inject(method = "randomTick", at = @At(target = "Lnet/minecraft/core/BlockPos;offset(III)Lnet/minecraft/core/BlockPos;", value = "INVOKE_ASSIGN"), locals = LocalCapture.CAPTURE_FAILSOFT)
-    private void onRandomTick(BlockState state, ServerLevel worldIn, BlockPos pos, RandomSource random, CallbackInfo cir, BlockState blockstate, int i, BlockPos blockpos) {
+    private void onRandomTick(BlockState pState, ServerLevel worldIn, BlockPos blockpos, RandomSource pRandom, CallbackInfo ci) {
         BlockState newBlock = worldIn.getBlockState(blockpos);
         if (newBlock.getBlock() instanceof RegolithBlock && RegolithGrassBlock.regolithToGrassMap.containsKey(newBlock.getBlock()) && RegolithGrassBlock.canPropagate(newBlock, worldIn, blockpos)) {
             if (!worldIn.getFluidState(blockpos.above()).is(FluidTags.WATER)) {
